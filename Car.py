9484a4f3d18d29b727b3d41cff785f5b17fa5aab
JuliaMariaLima <31971135+JuliaMariaLima@users.noreply.github.com>
2019-12-02 19:21:14 -0300

    Update Car.py

--- Car.py
+++ Car.py
@@ -2,15 +2,33 @@
 from flask import Flask, request, Response
 from gpiozero import Motor
 from time import sleep
+import RPi.GPIO as GPIO
+import json
 
 # criação do servidor
 app = Flask(__name__)
 
-@app.route("/seunome", methods=["GET","POST"])
-def juju():
+# criação de componentes
+leftWheel = Motor(23, 24)
+rightWheel = Motor(20, 21)
+ePin = 27
+dPin = 22
+ecounter = 0
+dcounter = 0
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(ePin, GPIO.IN)
+GPIO.setup(dPin, GPIO.IN)
+
+# definição de funcoes
+@app.route("/comandos", methods=["GET","POST"])
+def getPost():
     data = request.json
     print(request.form)
+    
     print(data)
+    desencoder(data)
     return Response(status=200)
 
 @app.route("/car/<string:y>")
@@ -18,11 +36,49 @@ def carAction(y):
     print(y)
     return y
 
-# criação de componentes
-leftWheel = Motor(23, 24)
-rightWheel = Motor(20, 21)
+def desencoder(data):
+    print(" >>>>> entrou <<<<<<")
 
-# definição de funcoes
+    ciclos = data['ciclos']
+    print("ciclos ", ciclos)
+    velocidade = 1#data['velocidade']
+    ## trocar isso daquiiii
+    print("velocidade " + str(velocidade))
+    for ciclo in ciclos:
+        print(ciclo)
+        movimentos = ciclo['movimentos']
+        print("movimentos ", movimentos)
+        vezes = ciclo['vezes']
+        print("vezes ", vezes)
+
+        for i in range(0, vezes):
+            for movimento in movimentos:
+                tipo = movimento["tipo"]
+                duracao = movimento["duracao"]
+                print(tipo)
+                if (tipo == "frente"):
+                    print("entrou frente")
+                    moveForward(velocidade)
+                elif (tipo == "tras"):
+                    moveBackward(velocidade)
+                elif (tipo == "direita"):
+                    turnRight()
+                elif (tipo == "esquerda"):
+                    turnLeft()
+                elif (tipo == "parar"):
+                    stop()
+                delay(duracao)
+
+def eEventRising(pin):
+    global ecounter
+    ecounter += 1
+    print("E COUNTER " + str(ecounter))
+    
+def dEventRising(pin):
+    global dcounter
+    dcounter += 1
+    print("D COUNTER " + str(dcounter))
+
 def stop():
     leftWheel.stop()
     rightWheel.stop()
@@ -43,6 +99,10 @@ def turnRight():
     leftWheel.forward()
     rightWheel.stop()
 
+
+GPIO.add_event_detect(ePin, GPIO.RISING, callback = eEventRising)
+GPIO.add_event_detect(dPin, GPIO.FALLING, callback = dEventRising)
+##moveForward(1)
 # loop infinito
 ##while True:
 ##    moveForward(0.5)
